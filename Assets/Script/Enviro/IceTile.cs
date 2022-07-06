ba915d0be503300f3ad7d411dcd71e3830d7ad38
Marzanos <punthita.marz@mail.kmutt.ac.th>
2022-07-06 16:48:01 +0700

    Ice_Tile

---
+++ Assets/Script/Enviro/IceTile.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceTile : MonoBehaviour
+{
+    PlayerStat playerstat;
+    private void Start()
+    {
+        playerstat = GameObject.FindObjectOfType<PlayerStat>();   
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            playerstat.moveSpeed = 5;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            StartCoroutine(slowDown(3f));
+            StartCoroutine(slowDown(2f));
+        }
+    }
+
+    IEnumerator slowDown(float speed)
+    {  
+        yield return new WaitForSeconds(1f);
+        playerstat.moveSpeed = speed;
+        
+    }
+}
